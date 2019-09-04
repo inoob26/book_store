e5b88bead3417fe9461d1778b498d0013eb0a3e8
inoob26 <morgunov@in-gr.ru>
2019-09-04 14:04:45 +0300

    add books db as json file

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify
+import json
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6\xabQ\x1f\xe8\xf3;\\,sW\x08\xb6\xff\xaecGO9\xadN[\xa7\xbf'
@@ -8,6 +10,13 @@ app.config['SECRET_KEY'] = '6\xabQ\x1f\xe8\xf3;\\,sW\x08\xb6\xff\xaecGO9\xadN[\x
 def home():
     return render_template('index.html')
 
+
+@app.route('/get_books')
+def get_books():
+    with open("json/books.json", mode='r', encoding='utf-8') as books:
+        data = json.load(books)
+    
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
